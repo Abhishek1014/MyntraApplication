c2c7f970c08910ab6df5301d56afff84076bdab4
Abhishek1014 <abhishekrana2637@gmail.com>
2020-05-05 01:03:46 +0530

    Second Push

--- myntra-service/src/main/java/com/upgrad/myntra/service/business/ItemService.java
+++ myntra-service/src/main/java/com/upgrad/myntra/service/business/ItemService.java
@@ -2,15 +2,54 @@ package com.upgrad.myntra.service.business;
 
 
 
+import com.upgrad.myntra.service.dao.CategoryDao;
+import com.upgrad.myntra.service.dao.ItemDao;
 import com.upgrad.myntra.service.entity.ItemEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /*
  * This ItemService interface gives the list of all the service that exist in the item service implementation class.
  * Controller class will be calling the service methods by this interface.
  */
-public interface ItemService {
+@Service
+public abstract class ItemService {
 
-    List<ItemEntity> getItemsByCategoryAndBrand(String brandId, String categoryId);
+    @Autowired
+    private ItemDao itemDao;
+
+    @Autowired
+    private CategoryDao categoryDao;
+
+
+
+
+    public List<ItemEntity> getItemsByCategoryAndBrand(String brandId, String categoryId)
+    {
+        List<ItemEntity> itemsOfbrand = (List<ItemEntity>) itemDao.getItemsByBrand(brandId);
+        // Get Items based on category uuid
+        List<ItemEntity> categoryItems = itemDao.getItemsByCategory(categoryId);
+        List<ItemEntity> categoryItemsOfBrand = new ArrayList<ItemEntity>();
+        if (itemsOfbrand != null) {
+            itemsOfbrand.forEach(item -> {
+                if (categoryItems != null) {
+                    for (ItemEntity categoryItem : categoryItems) {
+                        // Check if the item belongs to one of the items in this category
+                        if (item.getId() == categoryItem.getId()) {
+                            categoryItemsOfBrand.add(item);
+                            break;
+                        }
+                    }
+                }
+            });
+        }
+        return categoryItemsOfBrand;
+    }
+
+    public abstract ItemEntity getItemsByBrand(String brandId);
+
+    public abstract List<ItemEntity> getItemsByCategory(String categoryId);
 }
--- myntra-service/src/main/java/com/upgrad/myntra/service/dao/ItemDao.java
+++ myntra-service/src/main/java/com/upgrad/myntra/service/dao/ItemDao.java
@@ -1,14 +1,34 @@
 package com.upgrad.myntra.service.dao;
 
 import com.upgrad.myntra.service.entity.ItemEntity;
+import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 /*
  * This ItemDao interface gives the list of all the dao methods that exist in the item dao implementation class.
  * Service class will be calling the dao methods by this interface.
  */
-public interface ItemDao {
+@Repository
+public class ItemDao {
+    @PersistenceContext
+    private EntityManager entityManager;
 
-    List<ItemEntity> getItemsByCategoryAndBrand(String brandId, String categoryId);
+    public ItemEntity getItemsByBrand(String brandId)
+    {
+        try {
+            return entityManager.createNamedQuery("ItemsByBrand", ItemEntity.class).setParameter("brandId", brandId).getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+
+    public List<ItemEntity> getItemsByCategory(String categoryId)
+    {
+        return entityManager.createNamedQuery("ItemsByCategory", ItemEntity.class).setParameter("categoryId", categoryId).getResultList();
+
+    }
 }
